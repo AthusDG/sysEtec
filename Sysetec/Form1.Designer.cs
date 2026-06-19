156c2c581b5c4fcde46858329e49fc1eff18ce9e
AthusDG <athusdegomar5@gmail.com>
2026-06-19 08:20:44 -0300

    Add files via upload

---
+++ Sysetec/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Sysetec
+{
+    partial class Frmprincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            cadastrosToolStripMenuItem = new ToolStripMenuItem();
+            usuáriosToolStripMenuItem = new ToolStripMenuItem();
+            clientesToolStripMenuItem = new ToolStripMenuItem();
+            produtoToolStripMenuItem = new ToolStripMenuItem();
+            fornecedoresToolStripMenuItem = new ToolStripMenuItem();
+            movimentaçõesToolStripMenuItem = new ToolStripMenuItem();
+            entradaDeProdutosToolStripMenuItem = new ToolStripMenuItem();
+            saídaDeProdutosToolStripMenuItem = new ToolStripMenuItem();
+            relatóriosToolStripMenuItem = new ToolStripMenuItem();
+            clientesToolStripMenuItem1 = new ToolStripMenuItem();
+            produtosToolStripMenuItem = new ToolStripMenuItem();
+            vendasToolStripMenuItem = new ToolStripMenuItem();
+            utilitáriosToolStripMenuItem = new ToolStripMenuItem();
+            blocoDeNotasToolStripMenuItem = new ToolStripMenuItem();
+            calculadoraToolStripMenuItem = new ToolStripMenuItem();
+            catálogoDeProdutosToolStripMenuItem = new ToolStripMenuItem();
+            cotaçãoDeMoedasToolStripMenuItem = new ToolStripMenuItem();
+            ajudaToolStripMenuItem = new ToolStripMenuItem();
+            suporteToolStripMenuItem = new ToolStripMenuItem();
+            vídeosToolStripMenuItem = new ToolStripMenuItem();
+            sobreToolStripMenuItem = new ToolStripMenuItem();
+            sairToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { cadastrosToolStripMenuItem, movimentaçõesToolStripMenuItem, relatóriosToolStripMenuItem, utilitáriosToolStripMenuItem, ajudaToolStripMenuItem, sairToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1627, 33);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            cadastrosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { usuáriosToolStripMenuItem, clientesToolStripMenuItem, produtoToolStripMenuItem, fornecedoresToolStripMenuItem });
+            cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            cadastrosToolStripMenuItem.Size = new Size(107, 29);
+            cadastrosToolStripMenuItem.Text = "Cadastros";
+            // 
+            // usuáriosToolStripMenuItem
+            // 
+            usuáriosToolStripMenuItem.Name = "usuáriosToolStripMenuItem";
+            usuáriosToolStripMenuItem.Size = new Size(270, 34);
+            usuáriosToolStripMenuItem.Text = "Usuários";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            clientesToolStripMenuItem.Size = new Size(270, 34);
+            clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // produtoToolStripMenuItem
+            // 
+            produtoToolStripMenuItem.Name = "produtoToolStripMenuItem";
+            produtoToolStripMenuItem.Size = new Size(270, 34);
+            produtoToolStripMenuItem.Text = "Produto";
+            // 
+            // fornecedoresToolStripMenuItem
+            // 
+            fornecedoresToolStripMenuItem.Name = "fornecedoresToolStripMenuItem";
+            fornecedoresToolStripMenuItem.Size = new Size(270, 34);
+            fornecedoresToolStripMenuItem.Text = "Fornecedores";
+            // 
+            // movimentaçõesToolStripMenuItem
+            // 
+            movimentaçõesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { entradaDeProdutosToolStripMenuItem, saídaDeProdutosToolStripMenuItem });
+            movimentaçõesToolStripMenuItem.Name = "movimentaçõesToolStripMenuItem";
+            movimentaçõesToolStripMenuItem.Size = new Size(154, 29);
+            movimentaçõesToolStripMenuItem.Text = "Movimentações";
+            // 
+            // entradaDeProdutosToolStripMenuItem
+            // 
+            entradaDeProdutosToolStripMenuItem.Name = "entradaDeProdutosToolStripMenuItem";
+            entradaDeProdutosToolStripMenuItem.Size = new Size(278, 34);
+            entradaDeProdutosToolStripMenuItem.Text = "Entrada de produtos";
+            // 
+            // saídaDeProdutosToolStripMenuItem
+            // 
+            saídaDeProdutosToolStripMenuItem.Name = "saídaDeProdutosToolStripMenuItem";
+            saídaDeProdutosToolStripMenuItem.Size = new Size(278, 34);
+            saídaDeProdutosToolStripMenuItem.Text = "Saída de produtos";
+            // 
+            // relatóriosToolStripMenuItem
+            // 
+            relatóriosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clientesToolStripMenuItem1, produtosToolStripMenuItem, vendasToolStripMenuItem });
+            relatóriosToolStripMenuItem.Name = "relatóriosToolStripMenuItem";
+            relatóriosToolStripMenuItem.Size = new Size(106, 29);
+            relatóriosToolStripMenuItem.Text = "Relatórios";
+            // 
+            // clientesToolStripMenuItem1
+            // 
+            clientesToolStripMenuItem1.Name = "clientesToolStripMenuItem1";
+            clientesToolStripMenuItem1.Size = new Size(270, 34);
+            clientesToolStripMenuItem1.Text = "Clientes";
+            // 
+            // produtosToolStripMenuItem
+            // 
+            produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+            produtosToolStripMenuItem.Size = new Size(270, 34);
+            produtosToolStripMenuItem.Text = "Produtos";
+            // 
+            // vendasToolStripMenuItem
+            // 
+            vendasToolStripMenuItem.Name = "vendasToolStripMenuItem";
+            vendasToolStripMenuItem.Size = new Size(270, 34);
+            vendasToolStripMenuItem.Text = "Vendas";
+            // 
+            // utilitáriosToolStripMenuItem
+            // 
+            utilitáriosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { blocoDeNotasToolStripMenuItem, calculadoraToolStripMenuItem, catálogoDeProdutosToolStripMenuItem, cotaçãoDeMoedasToolStripMenuItem });
+            utilitáriosToolStripMenuItem.Name = "utilitáriosToolStripMenuItem";
+            utilitáriosToolStripMenuItem.Size = new Size(102, 29);
+            utilitáriosToolStripMenuItem.Text = "Utilitários";
+            // 
+            // blocoDeNotasToolStripMenuItem
+            // 
+            blocoDeNotasToolStripMenuItem.Name = "blocoDeNotasToolStripMenuItem";
+            blocoDeNotasToolStripMenuItem.Size = new Size(290, 34);
+            blocoDeNotasToolStripMenuItem.Text = "Bloco de notas";
+            // 
+            // calculadoraToolStripMenuItem
+            // 
+            calculadoraToolStripMenuItem.Name = "calculadoraToolStripMenuItem";
+            calculadoraToolStripMenuItem.Size = new Size(290, 34);
+            calculadoraToolStripMenuItem.Text = "Calculadora";
+            // 
+            // catálogoDeProdutosToolStripMenuItem
+            // 
+            catálogoDeProdutosToolStripMenuItem.Name = "catálogoDeProdutosToolStripMenuItem";
+            catálogoDeProdutosToolStripMenuItem.Size = new Size(290, 34);
+            catálogoDeProdutosToolStripMenuItem.Text = "Catálogo de produtos";
+            // 
+            // cotaçãoDeMoedasToolStripMenuItem
+            // 
+            cotaçãoDeMoedasToolStripMenuItem.Name = "cotaçãoDeMoedasToolStripMenuItem";
+            cotaçãoDeMoedasToolStripMenuItem.Size = new Size(290, 34);
+            cotaçãoDeMoedasToolStripMenuItem.Text = "Cotação de moedas";
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            ajudaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { suporteToolStripMenuItem, vídeosToolStripMenuItem, sobreToolStripMenuItem });
+            ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            ajudaToolStripMenuItem.Size = new Size(74, 29);
+            ajudaToolStripMenuItem.Text = "Ajuda";
+            // 
+            // suporteToolStripMenuItem
+            // 
+            suporteToolStripMenuItem.Name = "suporteToolStripMenuItem";
+            suporteToolStripMenuItem.Size = new Size(270, 34);
+            suporteToolStripMenuItem.Text = "Suporte";
+            // 
+            // vídeosToolStripMenuItem
+            // 
+            vídeosToolStripMenuItem.Name = "vídeosToolStripMenuItem";
+            vídeosToolStripMenuItem.Size = new Size(270, 34);
+            vídeosToolStripMenuItem.Text = "Vídeos";
+            // 
+            // sobreToolStripMenuItem
+            // 
+            sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            sobreToolStripMenuItem.Size = new Size(270, 34);
+            sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // sairToolStripMenuItem
+            // 
+            sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            sairToolStripMenuItem.Size = new Size(57, 29);
+            sairToolStripMenuItem.Text = "Sair";
+            sairToolStripMenuItem.Click += sairToolStripMenuItem_Click;
+            // 
+            // Frmprincipal
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1627, 597);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Frmprincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            WindowState = FormWindowState.Maximized;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem cadastrosToolStripMenuItem;
+        private ToolStripMenuItem usuáriosToolStripMenuItem;
+        private ToolStripMenuItem clientesToolStripMenuItem;
+        private ToolStripMenuItem produtoToolStripMenuItem;
+        private ToolStripMenuItem fornecedoresToolStripMenuItem;
+        private ToolStripMenuItem movimentaçõesToolStripMenuItem;
+        private ToolStripMenuItem entradaDeProdutosToolStripMenuItem;
+        private ToolStripMenuItem saídaDeProdutosToolStripMenuItem;
+        private ToolStripMenuItem relatóriosToolStripMenuItem;
+        private ToolStripMenuItem clientesToolStripMenuItem1;
+        private ToolStripMenuItem produtosToolStripMenuItem;
+        private ToolStripMenuItem vendasToolStripMenuItem;
+        private ToolStripMenuItem utilitáriosToolStripMenuItem;
+        private ToolStripMenuItem blocoDeNotasToolStripMenuItem;
+        private ToolStripMenuItem calculadoraToolStripMenuItem;
+        private ToolStripMenuItem catálogoDeProdutosToolStripMenuItem;
+        private ToolStripMenuItem cotaçãoDeMoedasToolStripMenuItem;
+        private ToolStripMenuItem ajudaToolStripMenuItem;
+        private ToolStripMenuItem suporteToolStripMenuItem;
+        private ToolStripMenuItem vídeosToolStripMenuItem;
+        private ToolStripMenuItem sobreToolStripMenuItem;
+        private ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
---
+++ Sysetec/Form1.cs
@@ -0,0 +1,15 @@
+namespace Sysetec
+{
+    public partial class Frmprincipal : Form
+    {
+        public Frmprincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
